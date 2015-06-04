09f11cae6587e18d8851423b6bdfe60fc8ebb2dc
akicho8 <akicho8@gmail.com>
2015-06-04 23:27:45 +0900

    Delete marubaru routing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy, :marubatu, :answered_counter_inc]
+  before_action :set_article, only: [:show, :edit, :update, :destroy, :answered_counter_inc]
 
   def index
     limit = 100
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :articles do
-    post :text_post, :on => :collection
-    patch :marubatu, :on => :member
     patch :answered_counter_inc, :on => :member
   end
 
