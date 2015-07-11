a5fc3fcdbe9b8e802fd26bda280b6055e12e1852
akicho8 <akicho8@gmail.com>
2015-07-11 20:44:53 +0900

    Add http_basic_authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,15 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  # if Rails.env.production?
-  #   # http_basic_authenticate_with name: "kotoba", password: "kotoba"
-  #
-  #   before_action do
-  #     authenticate_or_request_with_http_basic do |name, password|
-  #       name.to_s.match(/kotoba/) || password.to_s.match(/kotoba/)
-  #     end
-  #   end
-  # end
+  if Rails.env.production?
+    if v = ENV["HTTP_BASIC_AUTHENTICATE"].presence
+      name, password = v.split(/:/)
+      http_basic_authenticate_with name: name, password: password
+    end
+    # before_action do
+    #   authenticate_or_request_with_http_basic do |name, password|
+    #     name.to_s.match(/a/) || password.to_s.match(/a/)
+    #   end
+    # end
+  end
 end
